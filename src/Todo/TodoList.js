6815de7335caae020875fc5ed349e06bf927bafa
Dmitry <dmitrytbp@gmail.com>
2020-06-11 08:34:12 +0700

    Убрал toggleCheckBox из props

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TodoItem from './TodoItem/TodoItem'
+
 import PropTypes from 'prop-types'
 
 function TodoList(props){
@@ -9,10 +10,9 @@ function TodoList(props){
         props.todos.map((todo, index) => {
           return (
             <TodoItem 
-              todo={todo} 
-              key={todo.id} 
-              index={index} 
-              toggleCheckBox={props.toggleCheckBox}
+              todo={todo}
+              key={todo.id}
+              index={index}
             />
           )
         })
